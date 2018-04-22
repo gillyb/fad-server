7eaf9676b9b86e3e7008325e6ec13eb5f80c4cf0
gillyb <gillyb@gmail.com>
2018-04-22 23:19:43 +0300

    Change xml parser

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const xmlParser = require('xml2js');
 const prettyjson = require('prettyjson');
 const _ = require('lodash');
 const md5 = require('md5');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 const parser = new xmlParser.Parser({ charkey: 't' });
 
@@ -11,12 +13,10 @@ request.get('https://www.reddit.com/r/todayilearned/.rss', (err, response, body)
   // const data = JSON.parse(fs.readFileSync('json.js').toString());
   // console.log(body);
   parser.parseString(body, (err, result) => {
-    //console.log(prettyjson.render(result.feed.entry));
 
     const parsedFacts = {};
     result.feed.entry.forEach((e) => {
-      //console.log(e.content[0]);
-      //console.log('XXXXXX');
+
       let articleLink = /<a href=\"([a-zA-Z0-9\/\.\_\-\:\=\#\?\:\;%]+)\">\[link\]<\/a>/.exec(e.content[0].t);
       if (articleLink === null)
         return;
@@ -32,41 +32,58 @@ request.get('https://www.reddit.com/r/todayilearned/.rss', (err, response, body)
         fixedTitle = fixedTitle.replace('that ', '');
       if (fixedTitle.indexOf('That ') === 0)
         fixedTitle = fixedTitle.replace('That ', '');
+      if (fixedTitle.indexOf('Of ') === 0)
+        fixedTitle = fixedTitle.replace('Of ', '');
+      if (fixedTitle.indexOf('of ') === 0)
+        fixedTitle = fixedTitle.replace('of ', '');
 
       fixedTitle = fixedTitle.substr(0, 1).toUpperCase() + fixedTitle.substr(1);
 
       parsedFacts[factDate].push({
-        id: e.id,
+        id: e.id[0],
         customId: md5(e.title),
-        originalTitle: e.title,
+        originalTitle: e.title[0],
         title: fixedTitle,
         link: articleLink[1] 
       });
     });
-    console.log(prettyjson.render(parsedFacts));
 
     Object.keys(parsedFacts).forEach((d) => {
 
       const existing = { facts: [] };
       const filename = './data/' + d;
-      if (fs.existsSync(filename)) {
-        const old = JSON.parse(fs.readFileSync(filename).toString());
-        existing.facts = old.facts;
-      }
 
-      parsedFacts[d].forEach((f) => {
-        if (_.find(existing.facts, (a) => a.customId === f.customId) === undefined) {
-          existing.facts.push(f);
+      console.log('Looking for object: ' + d);
+      s3.getObject({ Bucket: 'fad-server', Key: d }, (err, file) => {
+        if (err)
+          console.log('Error: ' + JSON.stringify(err));
+
+        if (file) {
+          console.log('object ' + d + ' found');
+          const oldFacts = JSON.parse(file.Body.toString());
+          existing.facts = oldFacts;
         }
+
+        parsedFacts[d].forEach((f) => {
+          if (_.find(existing.facts, (a) => a.customId === f.customId) === undefined) {
+            existing.facts.push(f);
+          }
+        });
+
+        console.log('writing file: ' + d);
+        s3.putObject({
+          Bucket: 'fad-server',
+          Key: d,
+          Body: JSON.stringify(parsedFacts[d])
+        }, (err, data) => {
+          if (err)
+            console.log('error writing object: ' + d);
+          else
+            console.log('successfully written object: ' + d);
+        });
+
       });
-
-      // Save the facts to a file
-      fs.writeFileSync(filename, JSON.stringify(existing));
     });
-
-    // const filteredFacts = _.filter(interesting, (f) => !!f.link);
-
-    // console.log(prettyjson.render(filteredFacts));
 
   });
 });
